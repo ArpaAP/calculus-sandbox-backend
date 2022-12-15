cc12d6dec4a60181d5a8d4fb384ca7c89c884c77
알파 <arpa1705@gmail.com>
2022-12-15 14:53:42 +0900

    Initialize

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+from sympy import Symbol, SympifyError, integrate, sympify, latex
+from sympy.core.mul import Mul
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/integrate", methods=['POST'])
+def get_definite_integral():
+    data: dict = request.get_json()
+
+    print(data)
+
+    try:
+        parsed = sympify(data['expr'])
+    except:
+        return jsonify({'message': 'error'})
+
+    x = Symbol('x')
+
+    a = data.get('a', None)
+    b = data.get('b', None)
+
+    if isinstance(a, int) and isinstance(b, int):
+        try:
+            result = integrate(parsed, (x, a, b))
+        except:
+            return jsonify({'message': 'error'})
+
+        return jsonify({
+            'result_str': str(result),
+            'latex': latex(result)
+        })
+    else:
+        try:
+            result = integrate(parsed, (x,))
+        except:
+            return jsonify({'message': 'error'})
+
+        return jsonify({
+            'latex': latex(result).replace(' ', '')
+        }) 
+
+if __name__ == '__main__':
+    app.run()
